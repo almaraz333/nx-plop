25d98fde503296a1836b23268f2bfcbd183ba6c5
Colton Almaraz <almarazcolton333@gmail.com>
2022-05-24 16:17:45 -0700

    Fix: deleted intentional error to test husky

--- apps/react-template/src/app/components/TryAgain/TryAgainStyles.tsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-import { TryAgainProps } from './types';
-
-export const StyledTryAgain = styled.div<TryAgainProps>`
-  margin: 0;
-  margin: 0;
-`;
--- apps/react-template/src/app/components/TryAgain/index.ts
+++
@@ -1,1 +0,0 @@
-export { TryAgain } from './TryAgain';
