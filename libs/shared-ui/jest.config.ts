41807cad369b93dcc685b3bd4bea4eb34beb2f96
Colton Almaraz <almarazcolton333@gmail.com>
2022-05-25 14:00:51 -0700

    Feature: added generate library command

--- libs/shared-ui/jest.config.ts
+++ libs/shared-ui/jest.config.ts
@@ -2,13 +2,8 @@
 export default {
   displayName: 'shared-ui',
   preset: '../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
   transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/libs/shared-ui',
