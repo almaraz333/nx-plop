9154ddc64ac2da5d7b4368f56903265fb8f24a80
Colton Almaraz <almarazcolton333@gmail.com>
2023-04-27 10:12:51 -0700

    Removed references to styled components

--- apps/react-template/src/app/app.tsx
+++ apps/react-template/src/app/app.tsx
@@ -1,13 +1,10 @@
-import styled from 'styled-components';
 import NxWelcome from './nx-welcome';
-
-const StyledApp = styled.div``;
 
 export function App() {
   return (
-    <StyledApp>
+    <div>
       <NxWelcome title="react-template" />
-    </StyledApp>
+    </div>
   );
 }
 
